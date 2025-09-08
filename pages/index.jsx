2c987591646df94468afbafc0ac74c4016513021
Rwitapa <rwitapamitra1994@gmail.com>
2025-09-08 15:14:16 +0530

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { motion, AnimatePresence } from "framer-motion";
-import Image from 'next/image';
+//import Image from 'next/image';
 
 // --- CONSTANTS & UTILS ---
 
