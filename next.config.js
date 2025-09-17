81f5d877cacad897037848144e9b52cb71bc0061
Rwitapa <rwitapamitra1994@gmail.com>
2025-09-17 17:50:08 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,32 @@
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+
+const securityHeaders = [
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  }
+]
+
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
+};
+
+module.exports = nextConfig;
