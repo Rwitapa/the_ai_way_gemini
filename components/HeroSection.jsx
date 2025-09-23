9005250945374ee5526f4a3575acbd331af0d430
Rwitapa <rwitapamitra1994@gmail.com>
2025-09-24 04:22:19 +0530

    Update HeroSection.jsx

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -26,7 +26,7 @@ export const HeroSection = () => {
                         Gen AI for Analysts
                     </span>
 
-                    <h1 className="mt-6 text-4xl md:text-6xl lg:text-7xl font-extrabold leading-snug text-white max-w-5xl">
+                    <h1 className="mt-6 text-4xl md:text-6xl lg:text-7xl font-extrabold leading-normal text-white max-w-5xl">
                         Still stuck fixing reports?
                         <span className="block text-transparent bg-clip-text bg-gradient-to-r from-purple-300 via-purple-200 to-purple-100">
                             Build AI agents that decide.
