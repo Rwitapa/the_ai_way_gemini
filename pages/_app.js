5e3c8276c7b8399ae4d827c381384b009f815d0f
Rwitapa <rwitapamitra1994@gmail.com>
2025-09-23 21:50:56 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,33 @@
 // pages/_app.js
 import Head from "next/head";
 import { Analytics } from '@vercel/analytics/react';
+import { Inter, Poppins } from 'next/font/google';
 import "../styles/globals.css";
+
+// Configure the fonts
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+});
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['500'],
+  display: 'swap',
+  variable: '--font-poppins',
+});
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <>
+    // Add font variables to the main app container
+    <div className={`${inter.variable} ${poppins.variable}`}>
       <Head>
-        {/* Title shown in browser tab */}
         <title>The AI Way</title>
-
-        {/* Razorpay script added here */}
         <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
-
-        {/* Standard favicon for browsers */}
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="shortcut icon" href="/favicon.ico" />
-
-        {/* Apple Touch Icon for iOS home screen */}
         <link rel="apple-touch-icon" href="/logo_transparent.png" />
-
-        {/* Basic SEO meta tags */}
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta
           name="description"
@@ -29,6 +36,6 @@ export default function MyApp({ Component, pageProps }) {
       </Head>
       <Component {...pageProps} />
       <Analytics />
-    </>
+    </div>
   );
 }
