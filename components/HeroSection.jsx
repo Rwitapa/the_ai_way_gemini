93b0c17c843f8f9655b296d15fb034b49a519705
Rwitapa <rwitapamitra1994@gmail.com>
2025-09-24 09:44:45 +0530

    Update HeroSection.jsx

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -28,7 +28,7 @@ export const HeroSection = () => {
 
 
                     <h1 className="mt-6 text-4xl md:text-6xl lg:text-7xl font-extrabold leading-normal text-white max-w-5xl">
-                        Still stuck fixing reports?
+                        Still fixing reports manually?
                         <span className="block text-transparent bg-clip-text bg-gradient-to-r from-purple-300 via-purple-200 to-purple-100 pb-2">
                             Build AI agents that decide.
                         </span>
