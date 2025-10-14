5114a3e6957695b345b0504f7ec3e30bdf8cf4cd
Rwitapa <rwitapamitra1994@gmail.com>
2025-10-15 02:33:51 +0530

    Update HeroSection.jsx

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -1,4 +1,4 @@
-/ components/HeroSection.jsx
+// components/HeroSection.jsx
 import { motion } from "framer-motion";
 import React from 'react';
 import { WHATSAPP_COMMUNITY_URL } from '../lib/constants';
